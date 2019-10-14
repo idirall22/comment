cfc3a56bbec1548ef73499d8ea3f845635806207
idir <idirall22@gmail.com>
2019-10-14 18:00:43 +0100

    added comment models

---
+++ models/comment.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+// Comment structure
+type Comment struct {
+	ID        int64      `json:"id"`
+	Content   string     `json:"content"`
+	UserID    int64      `json:"user_id"`
+	Username  string     `json:"username"`
+	AvatarURL string     `json:"avatar_url"`
+	PostID    int64      `json:"post_id"`
+	CreatedAt time.Time  `json:"created_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
+}
