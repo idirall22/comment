06c81d1153ecec944f3f3ce208d98184735d29a3
idir <idirall22@gmail.com>
2019-10-17 16:21:57 +0100

    added handlers for add, update and delete comment

--- handlers.go
+++ handlers.go
@@ -1,12 +1,13 @@
 package comment
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 )
 
 // AddComment handler
-func AddComment(w http.ResponseWriter, r *http.Request) {
+func (s *Service) AddComment(w http.ResponseWriter, r *http.Request) {
 
 	form := CForm{}
 	err := json.NewDecoder(r.Body).Decode(&form)
@@ -14,21 +15,81 @@ func AddComment(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	if err != nil {
+		http.Error(w, "Could not add your comment", http.StatusBadRequest)
 		return
 	}
 
-	if !form.ValidateForm() {
+	ctx, f := context.WithTimeout(r.Context(), TimeoutRequest)
+	defer f()
+
+	c, err := s.addComment(ctx, form)
+
+	if err != nil {
+
+		message, code := parseError(err)
+		http.Error(w, message, code)
+
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(c); err != nil {
+		http.Error(w, "Error Serveur", http.StatusInternalServerError)
+		return
+	}
 }
 
 // UpdateComment handler
-func UpdateComment(w http.ResponseWriter, r *http.Request) {
+func (s *Service) UpdateComment(w http.ResponseWriter, r *http.Request) {
+	form := UForm{}
+	err := json.NewDecoder(r.Body).Decode(&form)
 
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w, "Could not add your comment", http.StatusBadRequest)
+		return
+	}
+
+	ctx, f := context.WithTimeout(r.Context(), TimeoutRequest)
+	defer f()
+
+	err = s.updateComment(ctx, form)
+
+	if err != nil {
+
+		message, code := parseError(err)
+		http.Error(w, message, code)
+
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNoContent)
 }
 
 // DeleteComment handler
-func DeleteComment(w http.ResponseWriter, r *http.Request) {
+func (s *Service) DeleteComment(w http.ResponseWriter, r *http.Request) {
 
+	commentID, err := parseURL(r)
+	if err != nil {
+		return
+	}
+
+	ctx, f := context.WithTimeout(r.Context(), TimeoutRequest)
+	defer f()
+
+	err = s.deleteComment(ctx, commentID)
+
+	if err != nil {
+
+		message, code := parseError(err)
+		http.Error(w, message, code)
+
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNoContent)
 }
