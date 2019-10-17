8dd297836b72dbb25836365d835760444cc4c324
idir <idirall22@gmail.com>
2019-10-17 17:26:09 +0100

    added parseURL func

---
+++ utils.go
@@ -0,0 +1,25 @@
+package comment
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+// parse url to get id
+func parseURL(r *http.Request) (int64, error) {
+
+	idString, ok := mux.Vars(r)["id"]
+
+	if !ok {
+		return 0, ErrorParam
+	}
+
+	id, e := strconv.ParseInt(idString, 10, 64)
+
+	if e != nil {
+		return 0, ErrorParam
+	}
+	return id, nil
+}
