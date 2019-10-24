6e1e75d1e7c4767852c9321755ad89553e4250e7
idir <idirall22@gmail.com>
2019-10-24 17:23:45 +0100

    Clean code

--- comments.go
+++ comments.go
@@ -2,22 +2,22 @@ package comment
 
 import (
 	"context"
-	"errors"
 
 	"github.com/idirall22/comment/models"
-	u "github.com/idirall22/user"
+	"github.com/idirall22/utilities"
 )
 
 // Add a comment
 func (s *Service) addComment(ctx context.Context, form CForm) (*models.Comment, error) {
 
-	if !form.ValidateForm() {
-		return nil, ErrorForm
+	userID, err := utilities.GetUserIDFromContext(ctx)
+
+	if err != nil {
+		return nil, err
 	}
 
-	userID, ok := ctx.Value(u.IDCtx).(int64)
-	if !ok {
-		return nil, errors.New("Error user id not valid")
+	if !form.ValidateForm() {
+		return nil, ErrorForm
 	}
 
 	c, err := s.provider.New(ctx, form.Content, userID, form.PostID)
@@ -32,13 +32,14 @@ func (s *Service) addComment(ctx context.Context, form CForm) (*models.Comment, 
 // Update a comment
 func (s *Service) updateComment(ctx context.Context, id int64, form CForm) (*models.Comment, error) {
 
-	if !form.ValidateForm() {
-		return nil, ErrorForm
+	userID, err := utilities.GetUserIDFromContext(ctx)
+
+	if err != nil {
+		return nil, err
 	}
 
-	userID, ok := ctx.Value(u.IDCtx).(int64)
-	if !ok {
-		return nil, errors.New("Error user id not valid")
+	if !form.ValidateForm() {
+		return nil, ErrorForm
 	}
 
 	return s.provider.Update(ctx, userID, id, form.Content)
@@ -47,9 +48,10 @@ func (s *Service) updateComment(ctx context.Context, id int64, form CForm) (*mod
 // Delete a comment
 func (s *Service) deleteComment(ctx context.Context, commentID int64) error {
 
-	userID, ok := ctx.Value(u.IDCtx).(int64)
-	if !ok {
-		return errors.New("Error user id not valid")
+	userID, err := utilities.GetUserIDFromContext(ctx)
+
+	if err != nil {
+		return err
 	}
 
 	return s.provider.Delete(ctx, userID, commentID)
