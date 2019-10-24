757630c62dadb792f2d29803472cb0ed68d77703
idir <idirall22@gmail.com>
2019-10-24 18:36:15 +0100

    Cleanned global_test file

--- global_test.go
+++ global_test.go
@@ -1,126 +1,57 @@
 package comment
 
 import (
-	"bytes"
 	"database/sql"
-	"encoding/json"
 	"fmt"
-	"log"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	pr "github.com/idirall22/comment/providers/postgres"
-	u "github.com/idirall22/user"
+	"github.com/idirall22/utilities"
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "password"
-	dbname   = "diskshar_test"
+var (
+	testService *Service
+	database    *sql.DB
+	testToken   string
+	tableName   = "comments"
+	query       = fmt.Sprintf(`
+	DROP TABLE IF EXISTS %s;
+
+	CREATE TABLE IF NOT EXISTS %s(
+		id SERIAL PRIMARY KEY,
+		content VARCHAR NOT NULL,
+		user_id INTEGER REFERENCES users(id),
+		post_id INTEGER REFERENCES posts(id),
+		created_at TIMESTAMP with TIME ZONE DEFAULT now(),
+		deleted_at TIMESTAMP DEFAULT NULL
+	);
+	`, tableName, tableName)
 )
 
-var testService *Service
-
-var testToken string
-var userUsernameTest = "alice"
-var userPasswordTest = "fdpjfd654/*sMLdf"
-
-func cleanDB(db *sql.DB) error {
-	query := fmt.Sprintf(`
-		DROP TABLE IF EXISTS comments;
-
-		CREATE TABLE IF NOT EXISTS comments(
-		    id SERIAL PRIMARY KEY,
-			content VARCHAR NOT NULL,
-			user_id INTEGER NOT NULL,
-			post_id INTEGER NOT NULL,
-		    created_at TIMESTAMP with TIME ZONE DEFAULT now(),
-		    deleted_at TIMESTAMP DEFAULT NULL
-		);
-		`)
-
-	_, err := db.Exec(query)
-
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func closeDB(db *sql.DB) {
-	db.Close()
-}
-
-// Get user auth allows to login and get a test token
-func getUserAuth(db *sql.DB) {
-
-	m := make(map[string]string)
-	m["username"] = userUsernameTest
-	m["password"] = userPasswordTest
-
-	b, err := json.Marshal(m)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	body := bytes.NewReader(b)
-
-	serviceUser := u.StartService(db, "users")
-
-	w := httptest.NewRecorder()
-	r, err := http.NewRequest("POST", "/login", body)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	h := http.HandlerFunc(serviceUser.Login)
-	h.ServeHTTP(w, r)
-
-	testToken = w.Header().Get("Authorization")
-}
-
-// Connect to db test
-func connectDB() error {
-
-	dbInfos := fmt.Sprintf(`host=%s port=%d user=%s password=%s dbname=%s sslmode=disable`,
-		host, port, user, password, dbname)
-
-	db, err := sql.Open("postgres", dbInfos)
-	if err != nil {
-		return err
-	}
-
-	provider := &pr.PostgresProvider{DB: db, TableName: "comments"}
-	testService = &Service{
-		provider: provider,
-	}
-	err = cleanDB(db)
-	if err != nil {
-		return err
-	}
-
-	getUserAuth(db)
-
-	testService = StartService(db)
-	return nil
-}
-
+// TestGlobal run tests
 func TestGlobal(t *testing.T) {
-	if err := connectDB(); err != nil {
-		log.Fatal("Error connect database test, ", err)
+
+	db, err := utilities.ConnectDataBaseTest()
+
+	if err != nil {
+		t.Error(err)
+		return
 	}
 
-	defer closeDB(testService.provider.(*pr.PostgresProvider).DB)
+	err = utilities.BuildDataBase(db, query)
 
-	// t.Run("add comment", testAddComment)
-	// t.Run("update a comment", testUpdateComment)
-	// t.Run("delete a comment", testDeleteComment)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	defer utilities.CloseDataBaseTest(db)
+
+	provider := &pr.PostgresProvider{DB: db, TableName: tableName}
+	testService = &Service{provider: provider}
+
+	testToken = utilities.LoginUser(db)
 
 	t.Run("add a comment handler", testAddCommentHandler)
 	t.Run("update a comment handler", testUpdateCommentHandler)
