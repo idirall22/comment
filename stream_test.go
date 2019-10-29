61f9280b14de3382984db95cc11648858c66943e
idir <idirall22@gmail.com>
2019-10-29 15:02:38 +0100

    Added test to stream.go

---
+++ stream_test.go
@@ -0,0 +1,33 @@
+package comment
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/idirall22/comment/brokers/memory"
+)
+
+// Test register client to stream
+func testSubscribeClientStream(t *testing.T) {
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	clientsLenght := testService.broker.(*memory.Memory).GetClientsLength
+
+	testService.subscribeClientStream(ctx)
+
+	if clientsLenght() != 1 {
+		t.Errorf("Clients length should be 1 but got %d", clientsLenght())
+	}
+
+	cancel()
+
+	time.Sleep(time.Millisecond)
+
+	defer func() {
+		if clientsLenght() != 0 {
+			t.Errorf("Clients length should be 0 but got %d", clientsLenght())
+		}
+	}()
+}
